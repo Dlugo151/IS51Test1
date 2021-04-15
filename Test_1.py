733021fa5aa2d3a9725e38f6d4d68465ca5363fc
Debora <deboribo@gmail.com>
2021-04-15 16:56:54 -0700

    finish structure english

--- Test_1.py
+++ Test_1.py
@@ -1,3 +1,17 @@
 
 
 
+"""
+The program will determin which of teh two options is better.
+The first option is 100 dollars per day for 10 days.
+The second option is 1 dollar a day and it doubles each day for 10 days.
+There will be to functions to calculate the pay rate.
+Fuction1 will calculate the amount for the first option; function2 will calculate the amount for te second option.
+
+fuction1 will output 100 * 10 days.
+fuction2 will loop through 10 times, each time it will be doubling the amount and will add the amount to the total.
+
+If the amount is equal, we output to th user "Option 1 and Option 2 pay the same"
+If the option1 is better, we output to the user "Option 1 is better"
+If the option2 is better, we output to the user "Option 2 is better"
+"""
